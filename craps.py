cfe6e1cc9dce46dd406320c27dfe2ebd41e4d73d
unknown <nersesyanvahe073@gmail.com>
2026-08-16 16:51:19 +0400

    Added Game logic with Class

--- craps.py
+++ craps.py
@@ -4,7 +4,7 @@ import time
 class Dice:
     def roll(self):
         print("Rolling the dice...")
-        time.sleep(1) 
+        time.sleep(1)
         
         die1 = random.randint(1, 6)
         die2 = random.randint(1, 6)
@@ -14,5 +14,43 @@ class Dice:
         return total
 
 
-my_dice = Dice() 
-result = my_dice.roll()
+class Game:
+    def __init__(self):
+        self.dice = Dice()
+
+    def play(self):
+        time.sleep(0.8)
+        print("--- WELCOME TO CRAPS ---")
+        input("Press Enter to make your first roll! ")
+        
+        first_roll = self.dice.roll()
+
+        if first_roll == 7 or first_roll == 11:
+            time.sleep(0.5)
+            print("Congratulations! You win on the first roll!")
+        elif first_roll == 2 or first_roll == 3 or first_roll == 12:
+            time.sleep(0.5)
+            print("Craps! The casino wins.")
+        else:
+            goal_number = first_roll
+            time.sleep(0.5)
+            print(f"\nYour GOAL number is now: {goal_number}")
+            time.sleep(0.5)
+            print("Keep rolling until you hit your goal to WIN. If you roll a 7, you LOSE.\n")
+            
+            while True:
+                time.sleep(0.5)
+                input("Press Enter to roll again... ")
+                next_roll = self.dice.roll()
+                
+                if next_roll == goal_number:
+                    print(f"You hit your goal of {goal_number}! YOU WIN!")
+                    break
+                elif next_roll == 7:
+                    print("Oh no, you rolled a 7! YOU LOSE!")
+                    break
+                else:
+                    print(f"You rolled a {next_roll}. Keep trying to hit your goal of {goal_number}.\n")
+
+my_game = Game()
+my_game.play()
